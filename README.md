12021b8de6e5613833e6442b828df4f557048a0e
Gene Kogan <kogan.gene@gmail.com>
2016-01-23 19:42:31 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# KinectOSC
+
+This is a simple utility for use with the Kinect. It will track a single user's skeleton and continuously send the coordinates over open sound control (OSC). 
+
+A gui gives you the option to toggle sending real-world (3d) coordinates or screen-projected (2d) coordinates or both.
+
+By default, it will stream to port 6448, and the addresses are:
+
+ - **/kinect/realworld** : real-world coordinates
+ - **/kinect/screen** : screen coordinates
+
+## release version
+
+A release is available for download in the [releases page](https://github.com/genekogan/KinectOSC/releases)
+
+## building from source
+
+If you want to build this from source, it requires [openFrameworks](https://www.openframeworks.cc), and the following addons:
+
+ - [ofxOpenNI](https://github.com/gameoverhack/ofxOpenNI/)
+ - [ofxControl](https://github.com/genekogan/ofxControl)
+
+Make sure you follow the instructions in [ofxOpenNI](https://github.com/gameoverhack/ofxOpenNI/) to include the openni config folder in the data folder of the application, which has been omitted from this repository because of space considerations.
---
+++ src/ofApp.cpp
@@ -0,0 +1,142 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    //ofSetDataPathRoot("../Resources/data/");
+    ofSetWindowShape(640, 480);
+
+    sendReal = true;
+    sendScreen = false;
+    
+    kinect.setup();
+    //kinect.setupFromONI("/Users/gene/Downloads/hometest_single.oni");
+    kinect.addDepthGenerator();
+    kinect.addUserGenerator();
+    kinect.setMaxNumUsers(1);
+    kinect.start();
+    
+    oscAddressRoot = OSC_ADDRESS_ROOT_DEFAULT;
+    oscPort = OSC_PORT_DEFAULT;
+    osc.setup("localhost", oscPort);
+    
+    gui.setName("KinectOSC");
+    gui.addToggle("send real-world coordinated", &sendReal);
+    gui.addToggle("send screen coordinates", &sendScreen);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofBackground(0);
+    
+    kinect.update();
+    if (!kinect.isNewFrame())   return;
+    
+    // trick to reset user generator if no tracked users found
+    if (kinect.getNumTrackedUsers() > 0) {
+        hadUsers = true;
+    }
+    else if (kinect.getNumTrackedUsers() == 0 && hadUsers){
+        hadUsers = false;
+        kinect.setPaused(true);
+        kinect.removeUserGenerator();
+        kinect.addUserGenerator();
+        kinect.setPaused(false);
+    }
+    
+    if (kinect.getNumTrackedUsers() == 0)   return;
+
+    // draw skeleton
+    kinect.drawSkeleton(0);
+    
+    
+    // send screen coordinates
+    if (sendScreen) {
+        ofxOscMessage msg;
+        msg.setAddress(oscAddressRoot+"/screen");
+        
+        ofxOpenNIUser & user = kinect.getTrackedUser(0);
+        for (int j=0; j<user.getNumJoints(); j++) {
+            ofxOpenNIJoint & joint = user.getJoint((Joint) j);
+            ofPoint p = joint.getProjectivePosition();
+            msg.addFloatArg(p.x);
+            msg.addFloatArg(p.y);
+        }
+        osc.sendMessage(msg);
+    }
+
+    // send real world coordinates
+    if (sendReal) {
+        ofxOscMessage msg;
+        msg.setAddress(oscAddressRoot+"/realworld");
+        
+        ofxOpenNIUser & user = kinect.getTrackedUser(0);
+        for (int j=0; j<user.getNumJoints(); j++) {
+            ofxOpenNIJoint & joint = user.getJoint((Joint) j);
+            ofPoint w = joint.getWorldPosition();
+            msg.addFloatArg(w.x);
+            msg.addFloatArg(w.y);
+            msg.addFloatArg(w.z);
+        }
+        osc.sendMessage(msg);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
